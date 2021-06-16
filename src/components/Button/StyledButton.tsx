6eaa63ecf6893a039f9240ccd278b2236ec1ca70
deazyyy <d.s@vimerco.com>
2021-06-16 19:49:10 +0530

    initial

---
+++ src/components/Button/StyledButton.tsx
@@ -0,0 +1,96 @@
+import styled, { DefaultTheme } from "styled-components";
+import { space } from "styled-system";
+import { ButtonProps, ButtonThemeVariant, variants } from "./types";
+
+type ThemedProps = {
+  theme: DefaultTheme;
+} & ButtonProps;
+
+const getDisabledStyles = ({ isLoading, theme }: ThemedProps) => {
+  if (isLoading === true) {
+    return `
+      &:disabled,
+      &.button--disabled {
+        cursor: not-allowed;
+      }
+    `;
+  }
+
+  return `
+    &:disabled,
+    &.button--disabled {
+      background-color: ${theme.colors.backgroundDisabled};
+      border-color: ${theme.colors.backgroundDisabled};
+      box-shadow: none;
+      color: ${theme.colors.textDisabled};
+      cursor: not-allowed;
+    }
+  `;
+};
+
+const removePointerEvents = ({ disabled, as }: ThemedProps) => {
+  if (disabled && as && as !== "button") {
+    return `
+      pointer-events: none;
+    `;
+  }
+
+  return "";
+};
+
+const getButtonVariantProp = (prop: keyof ButtonThemeVariant) => ({
+  theme,
+  variant = variants.PRIMARY,
+}: ThemedProps) => {
+  return theme.button[variant][prop];
+};
+
+const StyledButton = styled.button<ButtonProps>`
+  align-items: center;
+  background-color: ${getButtonVariantProp("background")};
+  border: 0;
+  border-radius: 16px;
+  box-shadow: ${getButtonVariantProp("boxShadow")};
+  color: ${getButtonVariantProp("color")};
+  cursor: pointer;
+  display: inline-flex;
+  font-family: inherit;
+  font-size: 14px;
+  font-weight: 600;
+  /* max-content instead of auto for Safari fix */
+  width: ${({ fullWidth }) => (fullWidth ? "100%" : "max-content")};
+  line-height: 1;
+  letter-spacing: 0.03em;
+  justify-content: center;
+  outline: 0;
+  padding: 14px 28px;
+  transition: background-color 0.2s;
+  opacity: ${({ isLoading }) => (isLoading ? 0.5 : 1)};
+  border:none !important;
+  outline:none !important;
+ 
+
+  &:hover:not(:disabled):not(.button--disabled):not(:active) {
+    outline:none;
+  }
+
+  &:focus:not(:active) {
+    border-color: none;
+    outline:none;
+  }
+
+  &:active {
+    box-shadow: none;
+  }
+
+  ${getDisabledStyles}
+  ${removePointerEvents}
+  ${space}
+`;
+
+StyledButton.defaultProps = {
+  fullWidth: false,
+  type: "button",
+};
+
+export default StyledButton;
